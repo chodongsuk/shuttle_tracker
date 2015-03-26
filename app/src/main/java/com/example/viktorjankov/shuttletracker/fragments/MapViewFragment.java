6ae3cbcd4881fb1a575a21607d4016ba4a721c33
viktorjankov <vj3836@truman.edu>
2015-03-25 21:54:03 -0700

    changed to smaller icons. design is done for now

--- app/src/main/java/com/example/viktorjankov/shuttletracker/fragments/MapViewFragment.java
+++ app/src/main/java/com/example/viktorjankov/shuttletracker/fragments/MapViewFragment.java
@@ -1,24 +1,20 @@
 package com.example.viktorjankov.shuttletracker.fragments;
 
 import android.annotation.TargetApi;
-import android.graphics.Color;
 import android.location.Location;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.viktorjankov.shuttletracker.BusProvider;
 import com.example.viktorjankov.shuttletracker.R;
-import com.example.viktorjankov.shuttletracker.directions.DirectionsJSONParser;
+import com.example.viktorjankov.shuttletracker.directions.DownloadTask;
 import com.example.viktorjankov.shuttletracker.directions.ParserTask;
 import com.example.viktorjankov.shuttletracker.model.DestinationLocation;
 import com.example.viktorjankov.shuttletracker.model.TravelMode;
@@ -30,20 +26,7 @@ import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.PolylineOptions;
 import com.squareup.otto.Bus;
-
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -51,9 +34,13 @@ import butterknife.OnClick;
 
 public class MapViewFragment extends Fragment {
 
-    @InjectView(R.id.header) TextView destination;
-    @InjectView(R.id.timeToDestination) TextView timeToDestination;
-    @InjectView(R.id.record_button) ImageButton mRecordButton;
+    private static final String DIRECTIONS_API_POINT = "https://maps.googleapis.com/maps/api/directions/";
+    @InjectView(R.id.header)
+    TextView destination;
+    @InjectView(R.id.timeToDestination)
+    TextView timeToDestination;
+    @InjectView(R.id.record_button)
+    ImageButton mRecordButton;
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @OnClick(R.id.record_button)
@@ -69,7 +56,8 @@ public class MapViewFragment extends Fragment {
         }
     }
 
-    @InjectView(R.id.mapview) MapView mapView;
+    @InjectView(R.id.mapview)
+    MapView mapView;
     GoogleMap map;
 
     DestinationLocation mDestination;
@@ -117,107 +105,15 @@ public class MapViewFragment extends Fragment {
         // Getting URL to the Google Directions API
         String url = getDirectionsUrl(origin, dest, mTravelMode);
 
-        DownloadTask downloadTask = new DownloadTask();
+        ParserTask parserTask = new ParserTask(map, timeToDestination);
+        DownloadTask downloadTask = new DownloadTask(map, parserTask);
 
-//        Start downloading json data from Google Directions API
+        // Start downloading json data from Google Directions API
         downloadTask.execute(url);
 
         return v;
     }
 
-    private String downloadUrl(String strUrl) throws IOException {
-        String data = "";
-        InputStream iStream = null;
-        HttpURLConnection urlConnection = null;
-        try{
-            URL url = new URL(strUrl);
-
-            // Creating an http connection to communicate with url
-            urlConnection = (HttpURLConnection) url.openConnection();
-
-            // Connecting to url
-            urlConnection.connect();
-
-            // Reading data from url
-            iStream = urlConnection.getInputStream();
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
-
-            StringBuffer sb  = new StringBuffer();
-
-            String line = "";
-            while( ( line = br.readLine())  != null){
-                sb.append(line);
-            }
-
-            data = sb.toString();
-
-            br.close();
-
-        }catch(Exception e){
-        }finally{
-            iStream.close();
-            urlConnection.disconnect();
-        }
-        return data;
-    }
-
-    // Fetches data from url passed
-    private class DownloadTask extends AsyncTask<String, Void, String> {
-
-        // Downloading data in non-ui thread
-        @Override
-        protected String doInBackground(String... url) {
-
-            // For storing data from web service
-            String data = "";
-
-            try{
-                // Fetching the data from web service
-                data = downloadUrl(url[0]);
-            }catch(Exception e){
-                Log.d("Background Task",e.toString());
-            }
-            return data;
-        }
-
-        // Executes in UI thread, after the execution of
-        // doInBackground()
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-
-            ParserTask parserTask = new ParserTask(map, timeToDestination);
-
-            // Invokes the thread for parsing the JSON data
-            parserTask.execute(result);
-        }
-    }
-
-    @Override
-    public void onResume() {
-        mapView.onResume();
-        bus.register(this);
-        super.onResume();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        mapView.onDestroy();
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapView.onLowMemory();
-    }
-
-    @Override
-    public void onPause() {
-        bus.unregister(this);
-        super.onPause();
-    }
 
     private String getDirectionsUrl(LatLng origin, LatLng dest, TravelMode travelMode) {
 
@@ -237,7 +133,26 @@ public class MapViewFragment extends Fragment {
         String output = "json";
 
         // Building the url to the web service
-        return  "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters;
+        return DIRECTIONS_API_POINT + output + "?" + parameters;
+    }
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        bus.register(this);
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 
     public void setDestination(DestinationLocation destinationLocation) {
